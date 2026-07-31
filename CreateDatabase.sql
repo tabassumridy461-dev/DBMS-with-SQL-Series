5824ecd4a42b22a977d07d6de8b67d5d03d2eef1
Tabassum Ridy <tabassumridy461@gmail.com>
2026-07-31 14:09:05 +0600

    Add SQL script to create and select StudentDB
    Basic SQL commands to create and use a database.

---
+++ CreateDatabase.sql
@@ -0,0 +1,17 @@
+
+ File: CreateDatabase.sql
+ Topic: Create Database
+ Description: Basic SQL commands to create
+              and use a database.
+
+// Create a new database
+CREATE DATABASE StudentDB;
+
+// Select the database for use
+USE StudentDB;
+
+//Show all available databases
+SHOW DATABASES;
+
+// Display the currently selected database
+SELECT DATABASE();
